25799e6e2f463f5cc0b1b7e6023810c620980b4f
Mark Diez <markediez@gmail.com>
2016-06-14 00:30:34 -0700

    Implement register user

--- projects/time-keeper/register.php
+++ projects/time-keeper/register.php
@@ -20,10 +20,11 @@
           <input class="col-md-12 form-control" type="text" placeholder="username" name="username" required>
           <input class="col-md-12 form-control form-item" type="password" placeholder="password" name="password" required>
           <input class="col-md-12 form-control form-item" type="text" placeholder="email" name="email" required>
-          <button class="col-md-4 col-md-offset-8 btn btn-primary form-item">Register</button>
+          <a class="col-md-4 col-md-offset-8 btn btn-primary form-item" onclick="postFormSubmit('#register-form', 'http://localhost:8888/db/ajax/register.php')">Register</a>
           <a class='col-md-4 col-md-offset-8 text-right no-padding' href="/">
             <span class="small">Already have an account? Login</span>
           </a>
+          <input type="submit" class="hide">
         </form>
       </div>
 

83dee19c44dec06c914075a2d5f588f4986372b2
Mark Diez <markediez@gmail.com>
2016-09-11 13:53:34 -0700

    Skeleton done

--- js/index.js
+++ js/index.js
@@ -2,3 +2,18 @@ function activateLink(el) {
   $(".active").removeClass("active");
   $(el).parent().addClass("active");
 }
+
+$(window).scroll(function() {
+  var projectOffset = $("#project").offset().top;
+  var contactOffset = $("#contact").offset().top;
+  var currentScroll = $(this).scrollTop();
+
+  if (currentScroll >= contactOffset - (projectOffset / 2) ) {
+    activateLink($("#link-contact"));
+  } else if (currentScroll >= projectOffset / 2) {
+    activateLink($("#link-project"));
+  } else {
+    activateLink($("#link-about"));
+  }
+
+});

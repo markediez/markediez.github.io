6c844024bc4f2cee56ccfc7ef0f931a3e1a35c13
Mark Diez <markediez@gmail.com>
2016-10-30 01:25:17 -0700

    Add JS file

---
+++ projects/free-code-camp/front-end-dev-certification/random-quote-machine/index.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+  changeQuote();
+});
+
+function changeQuote() {
+  var newQuote = {};
+
+  // Get quote from API
+  $.ajax({
+    type: "GET",
+    cache: false,
+    async: false,
+    url: "http://quotesondesign.com/wp-json/posts",
+    data: {"filter[orderby]": "rand", "filter[posts_per_page]": "1"},
+    success: function(result, textStatus, jqXHR) {
+      newQuote = result[0];
+    },
+    error: function(result, textStatus, jqXHR) {
+      console.log("ERROR");
+    }
+  });
+
+  // Change current quote to new quote
+  $("#random_quote .quote").html(newQuote["content"]);
+  $("#random_quote .author").html(newQuote["title"])
+}
+function tweet(quote, author) {
+  var quote = $("#random_quote .quote p").html() + " -" + $("#random_quote .author").html()
+  var url = "https://twitter.com/intent/tweet?hashtags=freecodecamp,quotes&text=";
+  url += quote;
+
+  window.open(url,'_blank');
+}
